7ab6023945f983683e635f8b7f14a21b3bedf2bf
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-08 23:19:05 +0600

    first commit

---
+++ ES6.js
@@ -0,0 +1,13 @@
+// let, const,var
+var age = 20;
+if(age > 18){
+    const name1 = "Salman Shah";
+    let name2 = "Manna";
+    var  name3 = "Riaz";
+    console.log(name1);
+    console.log(name2);
+    console.log(name3);
+}
+console.log(name1);
+console.log(name2);
+console.log(name3);
