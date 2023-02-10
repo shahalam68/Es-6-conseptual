ac3e3cc2c8d0ca378d16d7151290da2eacaa8f3d
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-10 19:29:56 +0600

    second

--- ES6.js
+++ ES6.js
@@ -1,13 +1,23 @@
 // let, const,var
 var age = 20;
 if(age > 18){
+    // const name1 = "Salman Shah";
+    // let name2 = "Manna";
+    // var  name3 = "Riaz";
+    // console.log(name1);
+    // console.log(name2);
+    // console.log(name3);
+}
+
+
+function mostan(){
+    
     const name1 = "Salman Shah";
     let name2 = "Manna";
     var  name3 = "Riaz";
-    console.log(name1);
-    console.log(name2);
-    console.log(name3);
 }
+
+mostan()
 console.log(name1);
 console.log(name2);
 console.log(name3);
