3770aa673057d1daf1151237c46299c805ac3658
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-15 21:51:48 +0600

    second

--- ES6.js
+++ ES6.js
@@ -1,23 +1,37 @@
-// let, const,var
-var age = 20;
-if(age > 18){
-    // const name1 = "Salman Shah";
-    // let name2 = "Manna";
-    // var  name3 = "Riaz";
-    // console.log(name1);
-    // console.log(name2);
-    // console.log(name3);
-}
+// // let, const,var
+// var age = 20;
+// if(age > 18){
+//     // const name1 = "Salman Shah";
+//     // let name2 = "Manna";
+//     // var  name3 = "Riaz";
+//     // console.log(name1);
+//     // console.log(name2);
+//     // console.log(name3);
+// }
 
 
-function mostan(){
+// function mostan(){
     
-    const name1 = "Salman Shah";
-    let name2 = "Manna";
-    var  name3 = "Riaz";
-}
+//     const name1 = "Salman Shah";
+//     let name2 = "Manna";
+//     var  name3 = "Riaz";
+// }
 
-mostan()
-console.log(name1);
-console.log(name2);
-console.log(name3);
+
+
+
+/* Default Parameter*/
+
+
+// function Mostan(name1, name2 = "bhai"){
+//     const fullName = `${name1} ${name2}`;
+//     return fullName;
+// }
+// console.log(mostan("bangla", "bhai"));
+
+
+function multi(firstPara, SecondPara =5){
+    const result = firstPara * SecondPara;
+    return result;
+}
+console.log(multi(5));
---
+++ soykot.js
@@ -0,0 +1,6 @@
+
+function multi(firstPara, SecondPara =5;){
+    const result = firstPara * SecondPara;
+    return result;
+}
+console.log(multi(5));
