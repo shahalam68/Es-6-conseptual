d1a1b91957d2bcc9b05ca3f5f954935fb388f54a
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-02-17 10:55:01 +0600

    template string

--- ES6.js
+++ ES6.js
@@ -30,8 +30,34 @@
 // console.log(mostan("bangla", "bhai"));
 
 
-function multi(firstPara, SecondPara =5){
+function multi(firstPara, SecondPara = 1){
     const result = firstPara * SecondPara;
     return result;
 }
-console.log(multi(5));
+// console.log(multi(5));
+
+/* Template String*/
+
+/*
+মাল্টি লাইন ব্যবহার, ডিয়নামিক ভাবে ভ্যালু এসাইন। ম্যাথমেটিক অপারেশান করা যায়
+*/
+
+const firstName =  "Ershad";
+const lastName = "Sikdar";
+let value = 2;
+// const fullName = firstName + " " + lastName;
+const fullName = `${firstName} ${lastName} ${value }`;
+// console.log(fullName);
+
+
+
+// const mostan = (name1, name2) =>{
+//     const fullName = `${name1} ${name2}`;
+//     return fullName;
+// }
+// console.log(mostan("Bangla", "Bhai"))
+
+/* Spread operator */
+const mostan = ['Josef', 'Bangla bhai','Ershad Sikdar','Sweden aslam'];
+console.log(...mostan);
+console.log(mostan);
--- soykot.js
+++
@@ -1,6 +0,0 @@
-
-function multi(firstPara, SecondPara =5;){
-    const result = firstPara * SecondPara;
-    return result;
-}
-console.log(multi(5));
